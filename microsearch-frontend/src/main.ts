35c1439a2dfe0f33762bad5e8363df8d64faba73
xu_xiaolong <luckily126163@126.com>
2023-05-09 23:40:49 +0800

    前端项目搭建

---
+++ microsearch-frontend/src/main.ts
@@ -0,0 +1,8 @@
+// 全局完整注册
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+
+createApp(App).use(Antd).use(router).mount("#app");
